4be8ce24e4431ab043094af069fbd4039f8b6920
이준호 <161573822+jhlee0227@users.noreply.github.com>
2024-05-27 13:48:06 +0900

    Update 치킨 쿠폰.java

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -11,7 +11,9 @@ class Solution {
             }
         }
         answer += coupon / 10;
+        // 마지막에 쿠폰이 10장 딱 맞춰지면 +1
         coupon += coupon / 10;
+        // 쿠폰이 0일때 +1이 되는 경우를 방지하기 위해 coupon > 1의 조건을 더함
         if (coupon % 10 == 0 && coupon > 1) {
             answer++;
         }
