f0cd207c0fccb7c5f8a090aa57bf372d52c113de
이준호 <161573822+jhlee0227@users.noreply.github.com>
2024-09-26 18:22:48 +0900

    [Bronze II] Title: 숫자의 개수, Time: 168 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        int[] arr = new int[10];
+        
+        int total = A * B * C;
+        
+        while (total > 0) {
+            arr[total % 10]++;
+            total /= 10;
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
